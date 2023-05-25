bef1b1e6d7e134f2a88d61967e5ac414ae494577
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-05-24 19:03:54 -0700

    Added prit_env.c file

---
+++ print_env.c
@@ -0,0 +1,15 @@
+#include "header_shell.h"
+
+/**
+ * print_env - prints the current environment
+ */
+void print_env(void)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
